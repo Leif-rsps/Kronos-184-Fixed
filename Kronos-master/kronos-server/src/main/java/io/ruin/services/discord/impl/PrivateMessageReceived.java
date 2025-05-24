75b5b0bd83d9266c960ff1bad8f51a5a90f3b982
Leif <leif.developer@proton.me>
2025-05-24 13:41:02 +0100

    Updated Gradle, Kotlin and JDA
    Updated Gradle to 8.14
    Updated Kotlin to 2.1.21
    Updated JDA to 5.5.1 and updated dependencies for JDA, Also renamed GuildMessageReceived and PrivateMessageReceived events to MessageReceived.

--- Kronos-master/kronos-server/src/main/java/io/ruin/services/discord/impl/PrivateMessageReceived.java
+++ Kronos-master/kronos-server/src/main/java/io/ruin/services/discord/impl/PrivateMessageReceived.java
@@ -4,18 +4,18 @@ package io.ruin.services.discord.impl;/*
  * Created on - 3/24/2020
  */
 
-//import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
-//import net.dv8tion.jda.api.hooks.ListenerAdapter;
-//
-//public class PrivateMessageReceived extends ListenerAdapter {
-//
-//    @Override
-//    public void onPrivateMessageReceived(PrivateMessageReceivedEvent e) {
-//
-//        if (e.getAuthor().isBot())
-//            return;
-//
-//        e.getChannel().sendMessage("Please use all commands in a public channel!").queue();
-//
-//    }
-//}
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class PrivateMessageReceived extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent e) {
+
+        if (e.getAuthor().isBot())
+            return;
+
+        e.getChannel().sendMessage("Please use all commands in a public channel!").queue();
+
+    }
+}
